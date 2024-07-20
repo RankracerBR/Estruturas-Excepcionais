e7a873959cb0cf722f324f9c3a120371249c772b
Augusto Pontes <augustopontes010@gmail.com>
2024-07-20 08:51:24 -0300

    Exceções

---
+++ src/AboutMe.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args){
+        try{
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US); // Scanner é um tipo de input
+
+            System.out.println("Digite o seu nome: ");
+            String nome = scanner.next();
+    
+            System.out.println("Digite o seu sobrenome: ");
+            String sobrenome = scanner.next();
+    
+            System.out.println("Digite a sua idade: ");
+            int idade = scanner.nextInt();
+            
+            System.out.println("Digite a sua altura");
+            double altura = scanner.nextDouble();
+
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
+
+        } catch (InputMismatchException e ){
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        }
+    }
+}
